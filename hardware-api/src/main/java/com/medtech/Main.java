8d7739164abb4eb59fa5c76903ea9d71edd1e242
Lucas Pagliarini <lucas.dsilva@sptech.school>
2024-06-11 15:46:44 -0300

    fix(jar)

--- hardware-api/src/main/java/com/medtech/Main.java
+++ hardware-api/src/main/java/com/medtech/Main.java
@@ -95,7 +95,7 @@ public class Main {
 
     private static void iniciarColetaDeDados(MonitoramentoMemoria memoria, MonitoramentoCpu cpu, Armazenamento armazenamento, MonitoramentoRede rede, ProcessosPC processos, ComponenteDAO componenteDAO, String nomeUsuario) throws SQLException {
         while (true) {
-            MemoryUsageFinisher.checkMemoryUsage();
+//            MemoryUsageFinisher.checkMemoryUsage();
             try {
                 Thread.sleep(3000);
                 Looca looca = new Looca();
